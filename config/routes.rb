d8eb31a51377afc3398e98a8f05a559ebcabcf7c
jey3033 <jey3033>
2023-02-06 11:49:48 +0700

    create room page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :room_messages
   resources :rooms
   devise_for :users
+
+  root controller: :room, action: :index
+
+  resource :room_messages
+  resource :rooms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
